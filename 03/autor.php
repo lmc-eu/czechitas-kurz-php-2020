e5b64f7498b4372002b8cbce447ae04835528068
Markéta Šebková <Marketa.Sebkova@lmc.eu>
2020-10-11 23:10:41 +0200

    Add exercise for associative arrays

---
+++ 03/autor.php
@@ -0,0 +1,16 @@
+<?php
+
+// Vytvořte asociativní pole $autorka, které bude obsahovat následující klíče s hodnotou:
+// - klíč "jmeno" a hodnota třeba "Emilka",
+// - klíč "pocetZhlednuti" a hodnota bude číslo (třeba mezi 0 a 300), můžeme ho měnit,
+// - klíč "hodnoceni" a hodnota zatím prázdný řetězec, tj. "".
+// Pokud má autorka méně než 50 zhlédnutí, chceme změnit hodnotu klíču "hodnoceni" na "neoblíbená", pokud 50 a více, změníme ho na "oblíbená".
+// Vypište podle počtu zhlédnutí větu "Emilka je oblíbená autorka" nebo "Emilka je neoblíbená autorka"
+
+$autorka = [
+    // ...
+];
+
+echo "DOPLŇTE_KDO je DOPLŇTE_JAKÁ autorka";
+
+?>
