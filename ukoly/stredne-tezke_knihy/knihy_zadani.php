114024029bc2c59d3fc7fe057d905efb300b3e3b
Markéta Šebková <Marketa.Sebkova@lmc.eu>
2020-10-27 17:55:24 +0100

    Add books exercise

---
+++ ukoly/stredne-tezke_knihy/knihy_zadani.php
@@ -0,0 +1,20 @@
+<?php
+
+$mojeKnihy = ["Tarzan", "Zlatovláska"];
+$knihyNaVymenu = ["Malenka", "Sněhurka"];
+
+// Vypište knihy z pole $mojeKnihy v HTML v seznamu (ul, li). Nevkládejte HTML značky v echu.
+
+?>
+
+<!-- ... řešení první části ... -->
+
+<?php
+
+// Vyměňte první knihu v poli $mojeKnihy za první knihu v poli $knihyNaVymenu. Nepoužívejte přímo hodnoty, použijte k výměně pouze proměnné!
+// (Představte si, že nevíte, jaké hodnoty jsou v poli uložené, víte jen, že chcete vyměnit druhý a třetí prvek v polích.)
+// Opět vypište pole $mojeKnihy. Použijte pro výpis všech HTML značek pžíkaz echo.
+
+// ... řešení druhé části ...
+
+?>
