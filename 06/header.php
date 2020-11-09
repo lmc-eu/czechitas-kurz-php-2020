035352f3f4d54118eaaa580d01ee55fe6d2d9dc0
Martin Holcman <martin.holcman@lmc.eu>
2020-11-09 20:32:00 +0100

    Lesson 6 result

---
+++ 06/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $nazevBlogu; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <!-- hlavička -->
+        <div class="header navbar navbar-light bg-light rounded">
+            <h1><?php echo $nazevBlogu; ?></h1>
+        </div>
