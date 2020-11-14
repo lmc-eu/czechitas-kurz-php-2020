b78ee81515fc4b4f5184ac2ded5c8f169d5b8b8c
Markéta Šebková <Marketa.Sebkova@lmc.eu>
2020-11-14 22:12:53 +0100

    Add solution of second exercise

--- 07/index.php
+++ 07/index.php
@@ -9,9 +9,22 @@ require 'header.php';
 <ul class="list-unstyled">
 
     <?php foreach ($clanky as $id => $clanek) { ?>
+
         <li class="jumbotron pt-2 pb-2 mt-3 mb-1">
             <strong><a href="clanek.php?id=<?php echo $id; ?>"><?php echo $clanek['nazev'] ?></a></strong>
             <span class="small"><?php echo $clanek['autor'] ?></span>
+            <p>
+            <?php
+                // echo mb_substr($clanek['obsah'], 0, 100);
+
+                // if (mb_strlen($clanek['obsah']) > 100) {
+                //     echo " ...";
+                // }
+
+                $odrazky = explode("<br />", $clanek['obsah']);
+                echo $odrazky[0] . " ...";
+            ?>
+            </p>
         </li>
     <?php } ?>
 
