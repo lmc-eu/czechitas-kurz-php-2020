0e886631bdc92e9472d1aab9c9e1e7867042ad94
Lukas <lukas.kalis@gmx.com>
2020-12-01 17:43:51 +0100

    pocitadlo reseni

--- 09/data.php
+++ 09/data.php
@@ -1,13 +1,12 @@
-<?php
+<?php session_start();
 $nazevBlogu = "Blog Czechitas";
 
 $clanky = unserialize(base64_decode(file_get_contents("clanky.txt")));
 
-$filename = 'pocitadlo.txt';
-
-if (file_exists($filename) == false) {
-    file_put_contents($filename, '1');
+if (!isset($_SESSION["zhlednuti"])) {
+    $_SESSION["zhlednuti"] = 1;
+} else {
+    $_SESSION["zhlednuti"] = $_SESSION["zhlednuti"] + 1;
 }
 
-$pocetZobrazeniStranky = file_get_contents($filename);
-file_put_contents($filename, $pocetZobrazeniStranky + 1);
+$pocetShlednutiStranky = $_SESSION["zhlednuti"];
