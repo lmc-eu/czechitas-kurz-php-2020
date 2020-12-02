6dda5c6e6f60b8b859900e3c934b02bcbfcf038b
Martin Holcman <martin.holcman@lmc.eu>
2020-12-02 19:14:52 +0100

    Kontrola existence session

--- 09/footer.php
+++ 09/footer.php
@@ -1,5 +1,8 @@
 <?php
-$navstiveneClanky = $_SESSION["navstiveno"];
+$navstiveneClanky = [];
+if (isset($_SESSION['navstiveno'])) {
+    $navstiveneClanky = $_SESSION["navstiveno"];
+}
 ?>
 
 <p>Navštívili jste již tyto články: <?php echo implode(",",$navstiveneClanky); ?></p>
